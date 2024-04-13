a99814ed02193870938b453abdc063ac043a96b6
niraj <nirajnimantha@gmail.com>
2024-04-14 01:46:54 +0530

    Add AuthContext component for managing authentication state

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,49 @@
+import React, { useContext, useEffect, useState } from "react";
+import { createContext } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem('user_data'));
+
+    if (storedData && storedData.userToken) {
+      setToken(storedData.userToken);
+      console.log('Tokenset',storedData.userToken);
+      setIsAuthenticated(true);
+      console.log('isAuthenticated',isAuthenticated);
+      setLoading(false);
+    }
+  }, []);
+
+  const login = (newToken) => {
+    const data = { userToken: newToken };
+    localStorage.setItem('user_data', JSON.stringify(data));
+    setToken(newToken);
+    setIsAuthenticated(true);
+  };
+
+  const logout = () => {
+    localStorage.removeItem('user_data');
+    setToken(null);
+    setIsAuthenticated(false);
+  };
+
+  const contextValue = {
+    token,
+    isAuthenticated,
+    loading,
+    login,
+    logout,
+  };
+
+  return (
+    <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
