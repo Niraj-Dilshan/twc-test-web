6d41dea7b9335653d545447fda7051999eadae9e
niraj <nirajnimantha@gmail.com>
2024-04-14 12:52:46 +0530

    Add BottomBar component for user logout functionality

---
+++ src/components/BottomBar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import exitIcon from '../assets/exitIcon.png';
+import { useAuth } from '../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+
+// The BottomBar component is a bar at the bottom of the screen that allows the user to log out.
+const BottomBar = () => {
+  const { logout, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  if (!isAuthenticated) {
+    navigate('/login');
+  }
+
+  return (
+    <div>
+      <button
+        onClick={() => logout()}
+        className="font-['poppins']  text-[1.438rem] leading-[3.125rem] text-white  underline cursor-pointer flex flex-row justify-between items-center gap-5"
+      >
+        <img src={exitIcon} width={43} height={43} />
+        logout
+      </button>
+    </div>
+  );
+};
+
+export default BottomBar;
