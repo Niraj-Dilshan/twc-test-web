2ee1b25294eced06c248c40239983b79973b5c46
niraj <nirajnimantha@gmail.com>
2024-04-15 22:13:45 +0530

    Add React components and styles for main app and authentication

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/auth/Login";
+import RootLayout from "./pages/RootLayout";
+import AuthRootLayout from "./pages/auth/AuthRootLayout";
+import Contacts from "./pages/Contacts";
+import Register from "./pages/auth/Register";
+import ErrorPage from "./pages/ErrorPage";
+import NewContact from "./pages/NewContact";
+import ProtectedRoutes from "./components/ProtectedRoutes";
+import "./App.css";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <RootLayout />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          index: true,
+          element: (
+            <ProtectedRoutes>
+              <Home />
+            </ProtectedRoutes>
+          ),
+        },
+        {
+          path: "contacts",
+          children: [
+            {
+              index: true,
+              element: (
+                <ProtectedRoutes>
+                  <Contacts />
+                </ProtectedRoutes>
+              ),
+            },
+            {
+              path: "new",
+              element: (
+                <ProtectedRoutes>
+                  <NewContact />
+                </ProtectedRoutes>
+              ),
+            },
+          ],
+        },
+        {
+          path: "login",
+          element: <AuthRootLayout />,
+          errorElement: <ErrorPage />,
+          children: [{ index: true, element: <Login /> }],
+        },
+        {
+          path: "register",
+          element: <AuthRootLayout />,
+          errorElement: <ErrorPage />,
+          children: [{ index: true, element: <Register /> }],
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
+}
+
+export default App;
